397fe1443dd89ed28813b496d18992ccdf8e4382
jeffykings <jeffykings@gmail.com>
2024-09-20 23:57:25 +0100

    window lasts long

--- events.c
+++ events.c
@@ -1,10 +1,31 @@
 #include "main.h"
 
 /**
- *  * handle_events - Stub function for handling events
- *   */
+ * handle_events - Basic event loop to keep the window open
+ */
+
 void handle_events()
 {
-	    /** Handle events here **/
-}
+	SDL_Event e;
+	int quit = 0;
 
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = 1;
+			}
+		}
+		
+		/** Clear screen **/
+		SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderClear(gRenderer);
+
+		/** Render objects here **/
+
+		/** Update screen **/
+		SDL_RenderPresent(gRenderer);
+	}
+}
