a1b72998a940a97fa1dba1534d762315186b529b
JK7 <99865958+jeffykings@users.noreply.github.com>
2024-09-25 00:01:02 +0100

    Update apply_wall_textures.c

--- apply_wall_textures.c
+++ apply_wall_textures.c
@@ -1,28 +1,18 @@
+
 #include "main.h"
 
 /**
- * apply_wall_textures - Renders wall textures based on raycasting results
- * @x: The x-coordinate on the screen
- * @wallHeight: The height of the wall slice to draw
- * @textureX: The x-coordinate on the texture
- * @wallType: The type of wall (for different textures)
+ * apply_wall_textures - Applies wall textures based on wall type
+ * @x: The x-coordinate to render the wall
+ * @wallHeight: The height of the wall
+ * @textureX: The X coordinate on the texture
+ * @wallType: The type of the wall (for different textures)
  */
-
 void apply_wall_textures(int x, int wallHeight, int textureX, int wallType)
 {
-	SDL_Rect srcRect, destRect;
+    SDL_Rect wallRect = {x, 480 - wallHeight, 1, wallHeight}; // Example calculation
 
-	srcRect.x = textureX;
-	srcRect.y = 0;
-	srcRect.w = 1;
-	srcRect.h = 64; /** Assuming texture height is 64 **/
+    SDL_Rect textureRect = {textureX, 0, 1, 64}; // Assuming 64 height texture
 
-	destRect.x = x;
-	destRect.y = (SCREEN_HEIGHT - wallHeight) / 2;
-	destRect.w = 1;
-	destRect.h = wallHeight;
-
-	SDL_Texture *currentTexture = wallTexture; /** Assuming one wall texture **/
-
-	SDL_RenderCopy(gRenderer, currentTexture, &srcRect, &destRect);
+    SDL_RenderCopy(gRenderer, wallTexture, &textureRect, &wallRect);
 }
